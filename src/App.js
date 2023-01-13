c93458b2894dbebd6f76de65d468125f91471723
steam88ys <82978463+steam88ys@users.noreply.github.com>
2023-01-13 09:38:59 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+    <div>
+      <h1>리액트 공부해보기!</h1>
+      <p>파일 구조를 파악해 봅시다.</p>
+    </div>
+  );
+}

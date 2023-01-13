bad929cbbf291f6b288f18189339e05dd8e9f1f2
steam88ys <82978463+steam88ys@users.noreply.github.com>
2023-01-13 10:03:07 +0900

    3 files modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,46 @@
 import React from 'react';
 import './style.css';
 
+function Header() {
+  return (
+    <header>
+      <h1>
+        <a href="/">WEB</a>
+      </h1>
+    </header>
+  );
+}
+function Nav() {
+  return (
+    <ol>
+      <li>
+        <a href="/read/1">html</a>
+      </li>
+      <li>
+        <a href="/read/2">css</a>
+      </li>
+      <li>
+        <a href="/read/3">js</a>
+      </li>
+    </ol>
+  );
+}
+function Article() {
+  return (
+    <article>
+      <h2>Welcome</h2>
+      Hello, WEB
+    </article>
+  );
+}
+
 export default function App() {
+  /*컴포넌트*/
   return (
     <div>
-      <h1>리액트 공부해보기!</h1>
-      <p>파일 구조를 파악해 봅시다.</p>
+      <Header></Header>
+      <Nav></Nav>
+      <Article></Article>
     </div>
   );
 }
